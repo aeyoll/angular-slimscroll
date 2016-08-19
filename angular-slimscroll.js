c10a3b48917f61dda8d1b887729f3a7503d0ed56
Jean-Philippe Bidegain <jp@bidega.in>
2016-08-19 09:26:03 +0200

    Wrap the directive in a IIFE

--- angular-slimscroll.js
+++ angular-slimscroll.js
@@ -1,66 +1,68 @@
-angular.module('ui.slimscroll', []).directive('slimscroll', ['$timeout', '$window', function($timeout, $window) {
-    'use strict';
+(function() {
+    angular.module('ui.slimscroll', []).directive('slimscroll', ['$timeout', '$window', function($timeout, $window) {
+        'use strict';
 
-    return {
-        restrict: 'A',
-        link: function($scope, $elem, $attr) {
-            var off = [];
-            var option = {};
+        return {
+            restrict: 'A',
+            link: function($scope, $elem, $attr) {
+                var off = [];
+                var option = {};
 
-            var refresh = function() {
-                $timeout(function() {
-                    if (angular.isDefined($attr.slimscroll)) {
-                        option = $scope.$eval($attr.slimscroll) || {};
+                var refresh = function() {
+                    $timeout(function() {
+                        if (angular.isDefined($attr.slimscroll)) {
+                            option = $scope.$eval($attr.slimscroll) || {};
+                        } else if ($attr.slimscrollOption) {
+                            option = $scope.$eval($attr.slimscrollOption) || {};
+                        }
+
+                        var el = angular.element($elem);
+
+                        el.slimScroll({
+                            destroy: true
+                        });
+                        el.slimScroll(option);
+                    });
+                };
+
+                angular.element($window).bind('resize', function() {
+                    if ($attr.slimscroll) {
+                        option = $scope.$eval($attr.slimscroll);
                     } else if ($attr.slimscrollOption) {
-                        option = $scope.$eval($attr.slimscrollOption) || {};
+                        option = $scope.$eval($attr.slimscrollOption);
                     }
 
-                    var el = angular.element($elem);
+                    $($elem).slimScroll(option);
+                });
 
-                    el.slimScroll({
+                var registerWatch = function() {
+                    if (angular.isDefined($attr.slimscroll) && !option.noWatch) {
+                        off.push($scope.$watchCollection($attr.slimscroll, refresh));
+                    }
+
+                    if ($attr.slimscrollWatch) {
+                        off.push($scope.$watchCollection($attr.slimscrollWatch, refresh));
+                    }
+
+                    if ($attr.slimscrolllistento) {
+                        off.push($scope.$on($attr.slimscrolllistento, refresh));
+                    }
+                };
+
+                var destructor = function() {
+                    angular.element($elem).slimScroll({
                         destroy: true
                     });
-                    el.slimScroll(option);
-                });
-            };
+                    off.forEach(function(unbind) {
+                        unbind();
+                    });
+                    off = null;
+                };
 
-            angular.element($window).bind('resize', function() {
-                if ($attr.slimscroll) {
-                    option = $scope.$eval($attr.slimscroll);
-                } else if ($attr.slimscrollOption) {
-                    option = $scope.$eval($attr.slimscrollOption);
-                }
+                off.push($scope.$on('$destroy', destructor));
 
-                $($elem).slimScroll(option);
-            });
-
-            var registerWatch = function() {
-                if (angular.isDefined($attr.slimscroll) && !option.noWatch) {
-                    off.push($scope.$watchCollection($attr.slimscroll, refresh));
-                }
-
-                if ($attr.slimscrollWatch) {
-                    off.push($scope.$watchCollection($attr.slimscrollWatch, refresh));
-                }
-
-                if ($attr.slimscrolllistento) {
-                    off.push($scope.$on($attr.slimscrolllistento, refresh));
-                }
-            };
-
-            var destructor = function() {
-                angular.element($elem).slimScroll({
-                    destroy: true
-                });
-                off.forEach(function(unbind) {
-                    unbind();
-                });
-                off = null;
-            };
-
-            off.push($scope.$on('$destroy', destructor));
-
-            registerWatch();
-        }
-    };
-}]);
+                registerWatch();
+            }
+        };
+    }]);
+})();
